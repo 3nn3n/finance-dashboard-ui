1a690ff2674d12d78a643219f571f066918d8c3c
Nitesh Negi <nitesh_negi@outlook.com>
2026-04-02 12:42:16 +0530

    Implemeted zustand store

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,19 @@
-import { mockTransactions } from "@/data/mockTransactions";
-import { getTotalBalance } from "@/lib/financeLogic";
+"use client";
+
+import { useFinanceStore } from "@/store/useFinanceStore";
 
 export default function Home() {
-  const balance = getTotalBalance(mockTransactions)
+  const totalBalance = useFinanceStore((state) => state.totalBalance());
+  const totalIncome = useFinanceStore((state) => state.totalIncome());
+  const totalExpenses = useFinanceStore((state) => state.totalExpenses());
   return (
     <>
-    <main className="p-6 m-8">
-      <h1 className="text-xl font-bold">Balance: Rs: {balance}</h1>
+    <main className="p-6 space-y-4">
+      <h1 className="text-2xl font-bold">Dashboard</h1>
+
+      <div>Balance: ₹{totalBalance}</div>
+      <div>Income: ₹{totalIncome}</div>
+      <div>Expenses: ₹{totalExpenses}</div>
     </main>
     </>
     
